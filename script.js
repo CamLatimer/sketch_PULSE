cbcc3612e81a18794548a0749e837bc2fef1b255
Cam <camlatimer@gmail.com>
2016-03-08 14:30:19 -0500

    still working

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-// goal:
-// on hover, cirlce grows then shrinks, then grows, then shrinks, then...
-  //select div and add even listener
+
   var pulseId;
-$('#circle-p').mouseenter(function() {
-  console.log('mouse entered <p>');
-  pulseId = setInterval(pulse);
+  var status = 0;
+$('#circle-p').click(function() {
+  if (status == 1) {
+    clearInterval(pulseId);
+  } else if (status == 0) {
+    pulseId = setInterval(pulse);
+  }
 });
-$('#circle-p').mouseout(function() {
-  console.log('mouse left <p>')
-  clearInterval(pulseId);
-})
-// to grow/shrink div on mouseenter,
-//  if it's a certain size, grow div
-//  if it's a certain size, shrink div
+
 function grow() {
   $('#circle-p').animate({
     width: '200px',
     height: '200px',
     borderRadius: '100px',
-  }, 100)
+  })
 }
 function shrink() {
   $('#circle-p').animate({
     width: '100px',
     height: '100px',
-  }, 100)
+  })
 }
 function pulse() {
   grow();
   shrink();
+  status = 1;
+}
+function stopPulse(id) {
+  console.log('interval cleared');
+  clearInterval(id);
+  status = 0;
 }
