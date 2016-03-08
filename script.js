94e7fa38d03b6bb326116b30d39011e2b87d518e
Cam <camlatimer@gmail.com>
2016-03-08 14:16:39 -0500

    made headway w/ animation

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // goal:
 // on hover, cirlce grows then shrinks, then grows, then shrinks, then...
   //select div and add even listener
-
+  var pulseId;
+$('#circle-p').mouseenter(function() {
+  console.log('mouse entered <p>');
+  pulseId = setInterval(pulse);
+});
+$('#circle-p').mouseout(function() {
+  console.log('mouse left <p>')
+  clearInterval(pulseId);
+})
 // to grow/shrink div on mouseenter,
 //  if it's a certain size, grow div
 //  if it's a certain size, shrink div
+function grow() {
+  $('#circle-p').animate({
+    width: '200px',
+    height: '200px',
+    borderRadius: '100px',
+  }, 100)
+}
+function shrink() {
+  $('#circle-p').animate({
+    width: '100px',
+    height: '100px',
+  }, 100)
+}
+function pulse() {
+  grow();
+  shrink();
+}
