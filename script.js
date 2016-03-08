6965d1aceaa3744ef15d0389201fb2fc98d765b4
Cam <camlatimer@gmail.com>
2016-03-08 13:26:15 -0500

    commit0 -- started idea

---
+++ script.js
@@ -0,0 +1,7 @@
+// goal:
+// on hover, cirlce grows then shrinks, then grows, then shrinks, then...
+  //select div and add even listener
+
+// to grow/shrink div on mouseenter,
+//  if it's a certain size, grow div
+//  if it's a certain size, shrink div
