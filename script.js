9d8d05c3e9d03baeb023a5e6ec6cdb6d2c2dda60
Cam <camlatimer@gmail.com>
2016-03-12 00:26:31 -0500

    got it looking good

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 var circle = $('#circle-p'); // select cirlce
 var iD;
-var status = 1;
+var status = 0;
 
 circle.hover(function(){
   pulse();
 
 }, function() {
-  clearInterval(iD)
+  clearTimeout(iD);
   circle.css({
     width: 100,
     height: 100,
-    transition: '1s'
   })
 })
 
@@ -18,7 +17,7 @@ function grow() {
   circle.css({
     width: 300,
     height: 300,
-    transition: '.1s'
+    transition: '.2s'
   })
   status = 1;
 }
@@ -27,17 +26,18 @@ function shrink() {
   circle.css({
     width: 100,
     height: 100,
-    transition: '.3s'
+    transition: '.2s'
   })
   status = 0;
 }
 
 function pulse() {
-  iD = setInterval(function() {
     if (status == 0) {
       grow()
     } else if (status == 1) {
       shrink()
     }
-  }, 300)
+  iD = setTimeout(function() {
+    pulse();
+  }, 200)
 }
